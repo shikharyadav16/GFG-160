1e3e29bd1e2ab096fee632c9ae9911eac3041952
Shikhar Yadav <92710243+shikharyadav16@users.noreply.github.com>
2025-09-10 16:14:27 +0530

    Create 03_anagram.py

---
+++ Strings/03_anagram.py
@@ -0,0 +1,24 @@
+# Check Anagram
+
+def checkAnagram(s1, s2):
+    temp = {}
+
+    for i in range(len(s1)):
+        temp[s1[i]] = temp.get(s1[i], 0) + 1
+
+    for i in range(len(s2)):
+        temp[s2[i]] = temp.get(s2[i], 0) - 1
+
+    for val in temp.values():
+        if (val != 0):
+            return False
+    return True
+
+s1 = input("Enter string 1:")
+s2 = input("Enter string 2:")
+
+if (checkAnagram(s1, s2)):
+    print("It is Anagram")
+else:
+    print("It is not Anagram")
+    
